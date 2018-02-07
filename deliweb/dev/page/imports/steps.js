888756b2b95ab25ca7159dd8c27d971291e22314
atl <atl@nbdeli.com>
2018-02-07 19:10:04 +0800

    重置git status 分支 dev 内容

--- deliweb/dev/page/imports/steps.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-define(['module', 'common/kernel/kernel', 'site/util/util', 'common/text/text!page/imports/steps.html!strip'], function(module, kernel, util, html) {
-	var userid = util.getCookie('userid'),
-        token = util.getCookie('token'),
-        orgid = util.getCookie('orgid');
-    var dom = $(html),
-        $imports = $('#imports .imports-box');
-    	$imports.append(dom);
-});
