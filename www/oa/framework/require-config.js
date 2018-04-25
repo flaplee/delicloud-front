ef4bf7aad474c2eadfeab6690fd5f9544ec9009a
atl <atl@nbdeli.com>
2018-04-25 14:30:30 +0800

    版本发布

--- www/oa/framework/require-config.js
+++ www/oa/framework/require-config.js
@@ -1,13 +1,13 @@
 'use strict';
 define(['module'], function(module) {
 	//请确保modules第一个被赋值
-	var modules = {"common/kernel":3,"common/slider":1,"common/text":1,"page/app":5,"page/appcap":2,"page/appdetail":2,"page/apphome":4,"page/base":2,"page/contacts":15,"page/department":1,"page/device":4,"page/home":3,"page/imports":17,"page/loginhome":13,"panel/adduser":1,"panel/editdept":1,"popup/adddept":1,"popup/deldept":1,"popup/deldevice":1,"popup/deluser":1,"popup/editdept":1,"popup/editdevice":1,"popup/finduser":1,"popup/loginInfo":1,"popup/loginPopup":1,"popup/renamedept":1,"popup/seluser":1,"site/import":1,"site/index":10,"site/pages":1,"site/panels":1,"site/popups":1,"site/util":9},
+	var modules = {"common/kernel":3,"common/slider":1,"common/text":1,"page/app":5,"page/appcap":2,"page/appdetail":2,"page/apphome":4,"page/base":2,"page/contacts":15,"page/department":1,"page/device":4,"page/home":3,"page/imports":18,"page/loginhome":13,"panel/adduser":1,"panel/editdept":1,"popup/adddept":1,"popup/deldept":1,"popup/deldevice":1,"popup/deluser":1,"popup/editdept":1,"popup/editdevice":1,"popup/finduser":1,"popup/loginInfo":1,"popup/loginPopup":1,"popup/renamedept":1,"popup/seluser":1,"site/import":1,"site/index":11,"site/pages":1,"site/panels":1,"site/popups":1,"site/util":9},
 		//请确保srcRoot第二个被赋值
 		srcRoot = 'dev/',
 		//请确保productRoot第三个被赋值
 		productRoot = 'dist/',
 		//请确保siteVersion第四个被赋值
-		siteVersion = "1.0.26",
+		siteVersion = "1.0.27",
 		//请确保debug第五个被赋值
 		debug = false,
 		prefix = module.id.replace(/framework\/[^\/]+$/, ''),
